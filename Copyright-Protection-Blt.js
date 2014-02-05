02ef0ee98107d1f2916cd4d9dfcd49df0825537e
bloggerlovetricks <bloggerlovetricks@gmail.com>
2014-02-05 23:13:21 +0700

    Create Copyright-Protection-Blt.js

---
+++ Copyright-Protection-Blt.js
@@ -0,0 +1,65 @@
+<!-- blogger Content Copy Protection (Copyright Protection) script by septian rs blt -->
+
+<div align="center"><noscript>
+   <div style="position:fixed; top:0px; left:0px; z-index:3000; height:100%; width:100%; background-color:#FFFFFF">
+   <div style="font-family: Tahoma; font-size: 14px; background-color:#0000ff; padding: 10pt;">Please enable your Javascript to see this page as it is meant to appear!</div></div>
+</noscript></div>
+
+<script type="text/javascript">
+window.onload = function() {
+	disableSelection(document.body)
+}
+ 
+function disableSelection(target){
+if (typeof target.onselectstart!="undefined")
+	target.onselectstart=function(){return false}
+else if (typeof target.style.MozUserSelect!="undefined")
+	target.style.MozUserSelect="none"
+else 
+	target.onmousedown=function(){return false}
+target.style.cursor = "default"
+}
+</script>
+
+<script type="text/javascript">
+function mousedwn(e) {
+try { if (event.button == 2||event.button == 3) return false; }
+catch (e) { if (e.which == 3) return false; }
+}
+	document.oncontextmenu = function() { return false; }
+	document.ondragstart   = function() { return false; }
+	document.onmousedown   = mousedwn;
+</script>
+
+<script type="text/javascript">
+if (top.location != self.location) top.location.replace(self.location);
+</script>
+
+<style type="text/css">
+* : (input, textarea) {
+
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+
+}
+
+img {
+	-webkit-touch-callout: none;
+	-webkit-user-select: none;		
+    }
+</style>
+
+<script type='text/javascript'>
+window.addEventListener("keydown",function (e) {
+    if (e.ctrlKey && (e.which == 65 || e.which == 67 || e.which == 85 || e.which == 80)) {
+        e.preventDefault();
+    }
+})
+        document.keypress = function(e) {
+        if (e.ctrlKey && (e.which == 65 || e.which == 67 || e.which == 85 || e.which == 80)) {
+	}
+		return false;
+				};
+</script>
+
+<!-- blogger Content Copy Protection (Copyright Protection) script by septian rs blt Ends -->
