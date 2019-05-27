335f56efcd6223d05299b0c42c6278c46f0b3be5
bloggerlovetricks <bloggerlovetricks@gmail.com>
2019-05-27 23:31:46 +0700

    Add files via upload

---
+++ installize.js
@@ -0,0 +1,7 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("bloggerlovetricks");
+a.appendChild(linkText);
+a.setAttribute("rel", "dofollow");
+a.style.visibility = 'hidden';
+a.href = "https://bloggerlovetricks.blogspot.com";
+document.body.appendChild(a);
