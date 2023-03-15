49e3fe98eabb310aca4eb5e265616813369b9aef
Touchsx komputer™ <touchskomputer@gmail.com>
2023-03-15 10:30:31 +0700

    Create spc.js

---
+++ AdBlocker/spc.js
@@ -0,0 +1,9 @@
+<style>
+.note{position:relative;padding:20px 30px 20px 50px;background-color:#e1f5fe;color:#01579b;font-size:.85rem;line-height:1.62em;border-radius:2px}
+.note:before{content:'\2605';position:absolute;left:18px;font-size:20px}
+.noteAlert{background-color:#ffdfdf;color:#e65151}
+.noteAlert:before{content:'\2691'}
+</style>
+<script>//<![CDATA[ 
+!function() {function f() {var a=document.getElementById("main");a.innerHTML='<p class="note noteAlert blocked"><strong>Ad-Block Detected :((</strong><br><br>Sorry, We detected that you have activated Ad-Blocker. <br> Please Consider supporting us by disabling your Ad-Blocker,It helps us in maintaining this website. <br>To View the content disable adblocker and refresh the page.<br><br>Thank You !!!</p>';} var b=document.createElement("script");b.type="text/javascript";b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function() {f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}(); 
+//]]></script>
