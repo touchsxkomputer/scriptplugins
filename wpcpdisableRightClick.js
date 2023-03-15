8070c0665abbc084f5446152e2c165a4dd2b93c2
Touchsx komputer™ <touchskomputer@gmail.com>
2023-03-16 05:22:04 +0700

    Create wpcpdisableRightClick.js

---
+++ wpcpdisableRightClick.js
@@ -0,0 +1,32 @@
+//<![CDATA[
+document.ondragstart = function() { return false;}
+	function nocontext(e) {
+	   return false;
+	}
+	document.oncontextmenu = nocontext;
+</script>
+<style>
+.unselectable
+{
+-moz-user-select:none;
+-webkit-user-select:none;
+cursor: default;
+}
+html
+{
+-webkit-touch-callout: none;
+-webkit-user-select: none;
+-khtml-user-select: none;
+-moz-user-select: none;
+-ms-user-select: none;
+user-select: none;
+-webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+</style>
+<script id="wpcp_css_disable_selection" type="text/javascript">
+var e = document.getElementsByTagName('body')[0];
+if(e)
+{
+	e.setAttribute('unselectable',"on");
+}
+//]]>
